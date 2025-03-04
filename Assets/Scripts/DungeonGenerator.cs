ab5972bc8ef8dc033338777e3cf52283785a1da1
Quince Weiss <quince.weiss@gmail.com>
2025-03-04 14:17:22 +0100

    Splitting rooms

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -1,19 +1,57 @@
 using UnityEngine;
 using Unity.VisualScripting;
+using System.Collections.Generic;
 
 public class DungeonGenerator : MonoBehaviour
 {
-    RectInt room = new RectInt(0, 0, 100, 50);
+    public List<RectInt> roomList = new List<RectInt>();
+
+    RectInt initalroom = new RectInt(0, 0, 100, 50);
+
     public float duration= 0;
     public bool depthTest = false;
     public float height = 0.0f;
+    public int rooms = 5;
 
     public AlgorithmsUtils algorithmsUtils;
 
     private void Update()
     {
-        AlgorithmsUtils.DebugRectInt(room, Color.green, duration, depthTest, height);
+        foreach (var room in roomList)  
+        {
+            AlgorithmsUtils.DebugRectInt(room, Color.green, duration, depthTest, height);
+        }
+        
+    }
+    private void Start()
+    {
+        roomList.Add(initalroom); // Ensure at least one initial room exists
+
+        for (int i = 0; i < rooms; i++)
+        {
+            if (roomList.Count > 0) // Ensure there are rooms to split
+            {
+                CreateRoom();
+            }
+        }
     }
 
-    
+    void CreateRoom()
+    {
+        int roomIndex = Random.Range(0, roomList.Count);
+        RectInt currentRoom = roomList[roomIndex];
+
+        int halfWidth = currentRoom.width / 2;
+
+        RectInt firstHalf, secondHalf;
+
+        int lineX = currentRoom.xMin + halfWidth;
+        firstHalf = new RectInt(currentRoom.xMin, currentRoom.yMin, halfWidth + 1, currentRoom.height);
+        secondHalf = new RectInt(lineX - 1, currentRoom.yMin, currentRoom.width - halfWidth + 1, currentRoom.height);
+
+        roomList.RemoveAt(roomIndex);
+        roomList.Add(firstHalf);
+        roomList.Add(secondHalf);
+
+    }
 }
