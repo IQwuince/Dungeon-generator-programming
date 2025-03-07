a17b163fec15db9cfa1cc3a73a2e5295840155a9
Quince Weiss <quince.weiss@gmail.com>
2025-03-07 19:29:22 +0100

    random rooms

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using Unity.VisualScripting;
 using System.Collections.Generic;
+using System.Collections;
 
 public class DungeonGenerator : MonoBehaviour
 {
@@ -13,16 +14,22 @@ public class DungeonGenerator : MonoBehaviour
     public float height = 0.0f;
     public int rooms = 5;
     public bool splitHorizontally;
+    public bool makeRoom;
+    private IEnumerator coroutine;
+
+    [Header("room size")]
+    public float minSizeX = 20;
+    public float minSizeY = 20;
+
 
     public AlgorithmsUtils algorithmsUtils;
 
     private void Update()
     {
-            foreach (var room in roomList)
-            {
-                AlgorithmsUtils.DebugRectInt(room, Color.green, duration, depthTest, height);
-            }
-
+        foreach (var room in roomList)
+        {
+            AlgorithmsUtils.DebugRectInt(room, Color.green, duration, depthTest, height);
+        }
     }
     private void Start()
     {
@@ -42,22 +49,40 @@ public class DungeonGenerator : MonoBehaviour
             }
         }
 
-        
+        //StartCoroutine(CreatingRooms());
     }
-
+    /*private IEnumerator CreatingRooms()
+    {
+        while (makeRoom == true)
+        {
+            foreach (var room in roomList)
+            {
+                AlgorithmsUtils.DebugRectInt(room, Color.green, duration, depthTest, height);
+            }
+            yield return new();
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                StartCoroutine(CreatingRooms());
+            }
+            
+        }
+    }*/
     void CreateRoom()
     {
         int roomIndex = Random.Range(0, roomList.Count);
         RectInt currentRoom = roomList[roomIndex];
 
         int halfWidth = currentRoom.width / 2;
-        int halfLength = currentRoom.height / 2; 
+        int halfLength = currentRoom.height / 2;
+
+        float surfaceRoom = currentRoom.width * currentRoom.height;
 
         RectInt firstHalf, secondHalf;
 
         int lineX = currentRoom.xMin + halfWidth;
         int lineY = currentRoom.yMin + halfLength;
-        if (splitHorizontally == true)
+
+        if (splitHorizontally == true && currentRoom.width > minSizeX && currentRoom.height > minSizeY)
         {
             firstHalf = new RectInt(currentRoom.xMin, currentRoom.yMin, currentRoom.width, halfLength + 1);
             secondHalf = new RectInt(currentRoom.xMin, lineY - 1, currentRoom.width, currentRoom.height - halfLength + 1);
@@ -65,8 +90,10 @@ public class DungeonGenerator : MonoBehaviour
             roomList.RemoveAt(roomIndex);
             roomList.Add(firstHalf);
             roomList.Add(secondHalf);
+            
+            splitHorizontally = false;
         }
-        else
+        else if (currentRoom.height > minSizeY && currentRoom.width > minSizeX)
         {
             firstHalf = new RectInt(currentRoom.xMin, currentRoom.yMin, halfWidth + 1, currentRoom.height);
             secondHalf = new RectInt(lineX - 1, currentRoom.yMin, currentRoom.width - halfWidth + 1, currentRoom.height);
@@ -74,6 +101,8 @@ public class DungeonGenerator : MonoBehaviour
             roomList.RemoveAt(roomIndex);
             roomList.Add(firstHalf);
             roomList.Add(secondHalf);
+
+            splitHorizontally = true;
         }
         
 
